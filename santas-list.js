06ec71f0497a938184f4e340abd8e8d3cd202b53
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-12-25 11:20:12 -0800

    isValid function to check for constraints

--- santas-list.js
+++ santas-list.js
@@ -5,3 +5,7 @@ function getRandomInt(min, max) {
 function moveItem(list, newIndex, oldIndex) {
   return list.splice(newIndex, 0, list.splice(oldIndex, 1)[0])
 }
+
+function isValid(key, testValue, invalid) {
+  return !(invalid[key] || []).includes(testValue)
+}
