08d643b01e9fc90e3cb5fb59cd0285d2ddd79e8c
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-12-25 11:19:45 -0800

    move list item function

--- santas-list.js
+++ santas-list.js
@@ -1,3 +1,7 @@
 function getRandomInt(min, max) {
   return min + Math.floor(Math.random() * (max - min));
 }
+
+function moveItem(list, newIndex, oldIndex) {
+  return list.splice(newIndex, 0, list.splice(oldIndex, 1)[0])
+}
